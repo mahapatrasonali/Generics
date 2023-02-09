7c77ef4371d7a925f6efb3ea8fe98fcc00d7050e
[mahapatrasonali] <[sonangr2000@gmail.com]>
2023-02-09 21:19:24 +0530

    find Maximum

---
+++ PrintArray.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    public class PrintArray<T>
+    {
+        private T[] inputarray;
+
+        public PrintArray(T[] inputArray)
+        {
+            this.inputarray = inputArray;
+        }
+
+        public void toPrint()
+        {
+            foreach (var element in inputarray)
+            {
+                Console.WriteLine(element);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Generics
+{
+    public class Program
+    {
+        public static void Main(string[] args) 
+        {
+            int num1 = 34;
+            int num2 = 20;
+            int value = num1.CompareTo(num2);
+            Console.WriteLine(value);
+            Console.ReadKey();
+        }
+    }
+}
